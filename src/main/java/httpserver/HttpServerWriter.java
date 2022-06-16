b5e7b63429f3ee310a0a983e778521835039cc12
Nicolas Piñeros Campo <onpc1224@gmail.com>
2022-06-15 21:00:34 -0500

    metodo para archivos de imagenes implementado

--- src/main/java/httpserver/HttpServerWriter.java
+++ src/main/java/httpserver/HttpServerWriter.java
@@ -66,7 +66,7 @@ public class HttpServerWriter {
             filein.close();
             DataOutputStream binaryOut = new DataOutputStream(clientSocket.getOutputStream());
             binaryOut.writeBytes("HTTP/1.0 200 OK\r\n");
-            binaryOut.writeBytes("Content-Type: image/png\r\n");
+            binaryOut.writeBytes("Content-Type: image/" + fileType + "\r\n");
             binaryOut.writeBytes("Content-Length: " + data.length);
             binaryOut.writeBytes("\r\n\r\n");
             binaryOut.write(data);
